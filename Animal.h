72b73a15a5c5ba371af27b9ff2233aee1d171752
MarawanElgendy <s7kp7tckp4@privaterelay.appleid.com>
2025-11-12 15:04:17 +0200

    Update Animal.h
    Completed Animal class

--- Animal.h
+++ Animal.h
@@ -1,11 +1,29 @@
+#include<string>
+using namespace std;
+
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-#include <string>
-using namespace std;
+class Animal{
+private:
+string name;
+int age;
+bool isHungry;
 
-//define animal class here 
+public:
+Animal();
+Animal(string, int, bool);
+~Animal();
 
+void setName(string);
+void setAge(int);
+void setIsHungry(bool);
 
+string getName();
+int getAge();
+bool getIsHungry();
 
+void display();
+void feed();
+};
 #endif
